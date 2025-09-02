ada1cca175449bccce78aee85c925362538ae598
toqa osama <82851804+toqaosama@users.noreply.github.com>
2025-09-02 11:38:36 +0300

    x

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Login from "./components/LoginPage";
-import Dashboard from "./components/Dashboard";
+import Login from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
 const queryClient = new QueryClient();
 
